56647e9411d84d3297856f6367ad6fef5167fa31
Vilius Sutkus <ViliusSutkus89@gmail.com>
2019-10-08 23:51:30 +0000

    Add basic usage instrumented tests
    Opening, closing, writing and reading back from tmpfile.

---
+++ android/tests/src/androidTest/java/com/viliussutkus89/tmpfile/tests/TmpfileInstrumentedTests.java
@@ -0,0 +1,62 @@
+package com.viliussutkus89.tmpfile.tests;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+import org.junit.FixMethodOrder;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+/*
+Sort tests by method name using @FixMethodOrder(MethodSorters.NAME_ASCENDING).
+Subsequent tests may crash, because they assume things from previous tests are working correctly.
+*/
+@RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TmpfileInstrumentedTests {
+  static {
+    System.loadLibrary("nativeUnitTests");
+  }
+
+  private File getTmpfileDir() {
+    final File cacheDir = InstrumentationRegistry.getInstrumentation().getTargetContext().getCacheDir();
+    return new File(cacheDir, "tmpfile");
+  }
+
+  @Test
+  public void a0_tmpfileDirCreated() {
+    final File tmpfileDir = getTmpfileDir();
+    assertTrue("Tmpfile directory does not exist!", tmpfileDir.exists());
+    assertTrue("Tmpfile directory is not actually a directory!", tmpfileDir.isDirectory());
+    assertTrue("Tmpfile directory is not readable!", tmpfileDir.canRead());
+    assertTrue("Tmpfile directory is not writable!", tmpfileDir.canWrite());
+  }
+
+
+  @Test
+  public void b1_openAndCloseTmpfile() {
+    assertTrue("Failed to open and close tmpfile!", open_and_close_tmpfile());
+  }
+
+  @Test
+  public void b2_writeToTmpfile() {
+    assertTrue("Failed to write to tmpfile!", write_to_tmpfile());
+  }
+
+  @Test
+  public void b3_writeAndReadbackFromTmpfile() {
+    assertTrue("Failed to read from previously written tmpfile!", write_and_readback_from_tmpfile());
+  }
+
+
+  private native boolean open_and_close_tmpfile();
+
+  private native boolean write_to_tmpfile();
+
+  private native boolean write_and_readback_from_tmpfile();
+}
---
+++ android/tests/src/main/cpp/nativeUnitTests.cpp
@@ -0,0 +1,109 @@
+#include <cerrno>
+#include <cstdio>
+#include <memory>
+#include <string_view>
+
+#include <jni.h>
+#include <android/log.h>
+
+const char * TAG = "TmpfileTests";
+const std::string_view sampleData = "Hello World";
+
+struct TmpFileAutoClose {
+  FILE * fh;
+
+  TmpFileAutoClose() {
+    fh = tmpfile();
+  }
+
+  bool close() {
+    bool retVal = true;
+    if (nullptr != fh) {
+      retVal = (0 == fclose(fh));
+    }
+    fh = nullptr;
+    return retVal;
+  }
+
+  ~TmpFileAutoClose() {
+    (void) close();
+  }
+};
+
+extern "C" {
+
+JNIEXPORT jboolean JNICALL
+Java_com_viliussutkus89_tmpfile_tests_TmpfileInstrumentedTests_open_1and_1close_1tmpfile(JNIEnv*, jobject) {
+  TmpFileAutoClose openedFile;
+
+  if (nullptr == openedFile.fh) {
+    __android_log_print(ANDROID_LOG_ERROR, TAG, "Failed to open tmpfile. errno: %d", errno);
+    return JNI_FALSE;
+  }
+
+  if (!openedFile.close()) {
+    __android_log_print(ANDROID_LOG_ERROR, TAG, "Failed to close previously opened tmpfile. errno: %d", errno);
+    return JNI_FALSE;
+  }
+
+  return JNI_TRUE;
+}
+
+JNIEXPORT jboolean JNICALL
+Java_com_viliussutkus89_tmpfile_tests_TmpfileInstrumentedTests_write_1to_1tmpfile(JNIEnv*, jobject) {
+  TmpFileAutoClose openedFile;
+
+  size_t written = 0;
+  while (written < sampleData.length()) {
+    int writtenThisChunk = fwrite(&sampleData.at(written), sizeof(sampleData.at(0)), sampleData.length() - written, openedFile.fh);
+    if (-1 == writtenThisChunk) {
+      __android_log_print(ANDROID_LOG_ERROR, TAG, "fwrite failed!");
+      return JNI_FALSE;
+    }
+    written += writtenThisChunk;
+  }
+
+  return JNI_TRUE;
+}
+
+JNIEXPORT jboolean JNICALL
+Java_com_viliussutkus89_tmpfile_tests_TmpfileInstrumentedTests_write_1and_1readback_1from_1tmpfile(JNIEnv*, jobject) {
+  TmpFileAutoClose openedFile;
+
+  size_t written = 0;
+  while (written < sampleData.length()) {
+    int writtenThisChunk = fwrite(&sampleData.at(written), sizeof(sampleData.at(0)), sampleData.length() - written, openedFile.fh);
+    if (-1 == writtenThisChunk) {
+      __android_log_print(ANDROID_LOG_ERROR, TAG, "fwrite failed!");
+      return JNI_FALSE;
+    }
+    written += writtenThisChunk;
+  }
+
+  if (0 != fseek(openedFile.fh, 0L, SEEK_SET)) {
+    __android_log_print(ANDROID_LOG_ERROR, TAG, "fseek failed! errno: %d", errno);
+    return JNI_FALSE;
+  }
+
+  char readBuffer[sampleData.length() + 1];
+  size_t haveRead = 0;
+  while (haveRead < sampleData.length()) {
+    int haveReadThisChunk = fread(&readBuffer[haveRead], sizeof(sampleData[0]), sampleData.length() - haveRead, openedFile.fh);
+    if (-1 == haveReadThisChunk) {
+      __android_log_print(ANDROID_LOG_ERROR, TAG, "fread failed!");
+      return JNI_FALSE;
+    }
+    haveRead += haveReadThisChunk;
+  }
+  readBuffer[sampleData.length()] = '\0';
+
+  if (0 != sampleData.compare(readBuffer)) {
+    __android_log_print(ANDROID_LOG_ERROR, TAG, "Data read from tmpfile does not match previously written sample data!");
+    return JNI_FALSE;
+  }
+
+  return JNI_TRUE;
+}
+
+
+}
