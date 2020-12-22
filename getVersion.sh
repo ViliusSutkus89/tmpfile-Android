9c33a8b624ab908b9ddf2e2b2a59ca477b8eef99
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2020-12-22 13:11:34 +0200

    Rename Android project from "lib" to "tmpfile"

--- getVersion.sh
+++ getVersion.sh
@@ -5,7 +5,7 @@ THIS_FILE=$(readlink -f "$0")
 BASEDIR=$(dirname "$THIS_FILE")
 
 # Extract project version from build.gradle
-gradle_file=$BASEDIR/lib/build.gradle
+gradle_file=$BASEDIR/tmpfile/build.gradle
 version_format='([0-9\.]+)'
 expression="s/^version \= ['\"]$version_format['\"]\$/\1/p"
 sed -En "--expression=$expression" $gradle_file
