9cdef76bdbcbf64918128675ccffed558f0952e8
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2021-05-25 13:44:13 +0300

    [issue #8] [WIP] Update CI pipeline to new template

--- scripts/prepareSampleAppForUnpublishedLib.sh
+++ scripts/prepareSampleAppForUnpublishedLib.sh
@@ -7,9 +7,8 @@ BASEDIR=$(readlink -f $(dirname "$THIS_FILE")/..)
 sed -E "/repositories \{/a mavenLocal()" -i $BASEDIR/sampleapp/build.gradle
 
 # Extract project version from build.gradle
-VERSION=$($BASEDIR/getVersion.sh)
+VERSION=$($BASEDIR/scripts/getVersion)
 version_format='([0-9\.]+)'
 # Update version in sample app's dependency list
 expression="s/implementation 'com.viliussutkus89:tmpfile-android:$version_format'/implementation 'com.viliussutkus89:tmpfile-android:$VERSION'/"
 sed -E "--expression=$expression" -i $BASEDIR/sampleapp/app/build.gradle
-
