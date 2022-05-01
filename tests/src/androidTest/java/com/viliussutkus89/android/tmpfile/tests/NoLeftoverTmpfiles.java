16e8f6f82898505ec75f5357d953832c3466a609
Vilius Sutkus '89 <ViliusSutkus89@gmail.com>
2022-05-01 16:40:02 +0300

    Fix issue in NoLeftoverTmpfiles test

--- tests/src/androidTest/java/com/viliussutkus89/android/tmpfile/tests/NoLeftoverTmpfiles.java
+++ tests/src/androidTest/java/com/viliussutkus89/android/tmpfile/tests/NoLeftoverTmpfiles.java
@@ -1,16 +1,23 @@
 package com.viliussutkus89.android.tmpfile.tests;
 
+import static org.junit.Assert.assertEquals;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.viliussutkus89.android.tmpfile.Tmpfile;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
-
 @RunWith(AndroidJUnit4.class)
 public class NoLeftoverTmpfiles {
+  @BeforeClass
+  public static void setUpClass() {
+    Tmpfile.init(InstrumentationRegistry.getInstrumentation().getTargetContext().getCacheDir());
+  }
+
   @Test
   public void noLeftoverTmpfiles() {
     assertEquals("Found leftover tmpfiles!", 0,
